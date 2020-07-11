b45fece345724919368ace97f65cad76276d47a8
Jason Campisi <aitsinformation@gmail.com>
2020-07-11 17:52:23 -0400

    v2
    Better parameter handling, error checking, add show name, more comments

--- gen_episodes.pl
+++ gen_episodes.pl
@@ -1,33 +1,58 @@
 #!/usr/bin/perl
 # Name: gen_episodes.pl
-# Version: 1
+# Version: 2
 # Author: Jason Campisi
-# Date: 7/10/2020
+# Date: 7/11/2020
 # Purpose: Generates a season and episode list template
 # License: Released under GPL v2 or higher. Details here http://www.gnu.org/licenses/gpl.html
-####
-#Ussage: gen_episodes.pl Number-Of-Seasons Number-Of-Episodes-Per-Seasons 
-# example: gen_episodes.pl 1 3
-# S01E01 -
-# S01E02 -
-# S01E03 -
 
 use strict;
+use Getopt::Long;
+
+my ($season, $episodes,$nameOfShow, $help) = (0,0,0,0);
+GetOptions("s:s"  =>\$season, "e:s" =>\$episodes, "n=s" =>\$nameOfShow, "help" =>\$help);
+
+sub check(){ # check required data or if help was called
+  if ($help or ($season <=0 or $episodes <=0) ){ 
+  print <<EOD;
+GenEpisodes Generates a season and episode list template
+    Ussage:             gen_episodes.pl -s=Seasons -e=Episodes
+    
+        -s=xxx          Number of Seasons you want to list 
+        -e=xxx          Number of Episodes you wish to have per season
+    
+    Optional:
+        -n=showName     This will place the name before the each episode line 
+        -help
+        
+Example: gen_episodes.pl 2 3 Nuts
+Nuts S01E01 -
+Nuts S01E02 -
+Nuts S01E03 -
+
+Nuts S02E01 -
+Nuts S02E02 -
+Nuts S02E03 -
+
+EOD
+    exit 0;    
+  }
+}#end Check
 
 sub pad($){ #add 0 padding???
   return ($_ >0 && $_ <10) ? "0$_" : $_;    #if 1->9 pad, otherwise nope  
 }#end pad
 
-if ($#ARGV !=1){ #Must have 2 arguments
-    print " Ussage requires 2 parameters:\n   gen_episodes.pl NumberOfSeasons NumberOfEpisodesPerSeason\n\n";
-    exit 0;
-}
+check();
 
-foreach(1..$ARGV[0]){ #loop for each season
+foreach(1..$season){ #loop for each season
     my $s=pad($_);
-    foreach (1..$ARGV[1]){ #loop for each episode
+    foreach (1..$episodes){ #loop for each episode
         my $ep=pad($_);
+        
+        print "$nameOfShow " if ($nameOfShow);
         print "S" . $s . "E" . $ep . " - \n";
+        
   }
   print "\n";    #add space between seasons
 }
